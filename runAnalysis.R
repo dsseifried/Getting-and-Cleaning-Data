70dd077c15374aaa9271af49c6320b941847fb1a
dsseifried <34424997+dsseifried@users.noreply.github.com>
2018-04-16 14:52:48 -0500

    Commit #4
    Finish tidydataset

--- runAnalysis.R
+++ runAnalysis.R
@@ -1,9 +1,4 @@
-
 # Load packages and Libraries
-
-#used to create codebook
-install.packages('dataMaid', dependencies = TRUE)
-library(dataMaid)   --#makeCodebook(dataset)
 
 #used to reshape data
 install.packages('dplyr', dependencies = TRUE)
@@ -30,6 +25,8 @@ actlabels[,2] <- as.character(actlabels[,2])
 #set feature dataset and change 2nd column from factory to characters
 tempfeat <-read.table('./Class3Week4/UCI HAR Dataset/features.txt')
 tempfeat[,2]<-as.character(tempfeat[,2])
+
+#select any columns with the term 'mean' or 'std' in the name STEP 2
 reqfeatures <- subset(tempfeat, grepl(".*mean.*|.*std.*", tempfeat$V2))
 
 #Load (Train) - hint forum (thoughtfulbloke)- use cbind  
@@ -38,34 +35,31 @@ ytrain <- read.table('./Class3Week4/UCI HAR Dataset/train/y_train.txt')
 subtrain <- read.table('./Class3Week4/UCI HAR Dataset/train/subject_train.txt')
 newtrain<-cbind(subtrain,ytrain,xtrain)
 
-#Change/Simplify Column Names (Train)
-colnames(newtrain)<-c("Subject","Activity",reqfeatures$V2)
-colnames(newtrain)<-gsub("-mean", "Mean", colnames(newtrain))
-colnames(newtrain)<-gsub("-std", "Std", colnames(newtrain))
-colnames(newtrain)<-gsub("()-", "", colnames(newtrain))
-colnames(newtrain)<-gsub("[()]", "", colnames(newtrain))
-
 #Load (Test) - hint forum (thoughtfulbloke)- use cbind 
 xtest <- read.table('./Class3Week4/UCI HAR Dataset/test/x_test.txt')[reqfeatures$V1]
 ytest <- read.table('./Class3Week4/UCI HAR Dataset/test/y_test.txt')
 subtest <- read.table('./Class3Week4/UCI HAR Dataset/test/subject_test.txt')
 newtest<-cbind(subtest,ytest,xtest)
 
-#Change/Simplify Column Names (Test)
-colnames(newtest)<-c("Subject","Activity",reqfeatures$V2)
-colnames(newtest)<-gsub("-mean", "Mean", colnames(newtest))
-colnames(newtest)<-gsub("-std", "Std", colnames(newtest))
-colnames(newtest)<-gsub("()-", "", colnames(newtest))
-colnames(newtest)<-gsub("[()]", "", colnames(newtest))
-
-#Merge Test and Train , hints use rbind
+#Merge Test and Train , hints use rbind STEP 1
 fulldata <- rbind(newtrain,newtest)
 
-#update activities to be descriptive in full dataset 
+#update activities to be descriptive in full dataset STEP 3
 fulldata$Activity <- actlabels$V2[match(fulldata$Activity, actlabels$V1)]
-#table(fulldata$Activity)
 
+#Change/Simplify Column Names (fulldata) STEP 4
+colnames(fulldata)<-c("Subject","Activity",reqfeatures$V2)
+colnames(fulldata)<-gsub("-mean", "Mean", colnames(fulldata))
+colnames(fulldata)<-gsub("-std", "Std", colnames(fulldata))
+colnames(fulldata)<-gsub("()-", "", colnames(fulldata))
+colnames(fulldata)<-gsub("[()]", "", colnames(fulldata))
 
+#STEP 5
+tidydataset <- 
+      fulldata %>%
+      group_by(Activity,Subject) %>%
+      summarize_all(funs(mean))
 
-
+#Write the dataset to a file.
+write.table(tidydataset,file = './Class3Week4/tidydataset.txt',row.names = FALSE)
 
